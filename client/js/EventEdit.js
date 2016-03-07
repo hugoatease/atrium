320ad5bda1e7d77feb0215d1815882b4129c8be4
Hugo Caille <hugo.caille@isen-lille.fr>
2016-03-07 15:07:08 +0100

    Event deletion

--- client/js/EventEdit.js
+++ client/js/EventEdit.js
@@ -1,14 +1,27 @@
 var React = require('react');
 var EventForm = require('./EventForm');
 var EventPoster = require('./EventPoster');
+var request = require('superagent');
+var browserHistory = require('react-router').browserHistory;
+
 
 var EventEdit = React.createClass({
+    remove: function() {
+        request.del('/api/events/' + this.props.params.event_id)
+            .end(function(err, res) {
+                if (err) return;
+                browserHistory.goBack();
+            }.bind(this));
+    },
+
     render: function() {
+        console.log(this.props);
         return (
             <div className="row">
                 <div className="medium-6 column">
                     <h5>Event details</h5><hr />
                     <EventForm event_id={this.props.params.event_id} />
+                    <button onClick={this.remove} className="button alert">Delete event</button>
                 </div>
                 <div className="medium-6 column">
                     <h5>Event poster</h5><hr />
