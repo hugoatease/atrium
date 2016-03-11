a2c5b4d3e4e48ca6e4fae096c19c70affcc09501
Hugo Caille <hugo.caille@isen-lille.fr>
2016-03-11 12:38:19 +0100

    Removed debug code

--- atrium/api/event.py
+++ atrium/api/event.py
@@ -84,7 +84,6 @@ class EventResource(Resource):
 
         if 'description' in args.keys():
             event.description = bleach.clean(args['description'], tags=ALLOWED_TAGS, styles=ALLOWED_STYLES, attributes=ALLOWED_ATTRIBUTES)
-            print event.description
 
         if 'place' in args.keys():
             event.place = Place(name=args['place']['name'], address=args['place']['address'])
